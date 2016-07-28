5cd431987a5baeedc6ae6998f65689aae05cbe8a
Marc Obel <marcentusch@hotmail.com>
2016-07-28 13:31:32 +0200

    Added start class

---
+++ src/SnakeWindow.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Marc on 28-07-2016.
+ */
+public class SnakeWindow extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Snake");
+
+
+
+    }
+}
